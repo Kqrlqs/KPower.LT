4b118b69458b492040e25074d5919ab47e5616be
Kqrlqs <marmakarolis20@gmail.com>
2025-04-26 12:50:14 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,25 +1,14 @@
-<!DOCTYPE html>
-<html lang="lt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Prisijungimas</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <h1>Admin Prisijungimas</h1>
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-    <form id="loginForm">
-        <label for="username">Vartotojo vardas:</label>
-        <input type="text" id="username" required><br><br>
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
 
-        <label for="password">Slaptažodis:</label>
-        <input type="password" id="password" required><br><br>
+    if (username === 'Karlas' && password === 'Bmwe70') {
+        window.location.href = 'admin.html';
+    } else {
+        alert('Neteisingas vartotojo vardas arba slaptažodis');
+    }
+});
 
-        <button type="submit">Prisijungti</button>
-    </form>
-
-    <script src="login.js"></script>
-</body>
-</html>
 
