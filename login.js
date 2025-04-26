754f763a2cefc842b28858b5696baa9fb7173452
Kqrlqs <marmakarolis20@gmail.com>
2025-04-26 13:05:23 +0300

    Update login.js

--- login.js
+++ login.js
@@ -2,15 +2,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginForm = document.getElementById('loginForm');
 
     loginForm.addEventListener('submit', function(e) {
-        e.preventDefault(); // Sustabdome automatinį formos siuntimą
+        e.preventDefault();
 
         const username = document.getElementById('username').value.trim();
         const password = document.getElementById('password').value.trim();
 
         if (username === 'Karlas' && password === 'Bmwe70') {
-            // Įrašome, kad vartotojas prisijungęs
             localStorage.setItem('isLoggedIn', 'true');
-            // Peradresuojame į admin.html puslapį
             window.location.href = 'admin.html';
         } else {
             alert('Neteisingas vartotojo vardas arba slaptažodis!');
