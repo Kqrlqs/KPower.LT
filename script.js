f998059ce72e1b580eae80f19b5d3bd21bd19e7d
Kqrlqs <marmakarolis20@gmail.com>
2025-04-26 12:39:37 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+document.getElementById('diagnosticsForm').addEventListener('submit', function(event) {
+    const carModel = document.getElementById('carModel').value;
+    const customerName = document.getElementById('customerName').value;
+    const phone = document.getElementById('phone').value;
+    const diagnosticDate = document.getElementById('diagnosticDate').value;
+    const diagnosticTime = document.getElementById('diagnosticTime').value;
+    const service = document.getElementById('service').value;
 
-const scriptURL = 'AKfycbweNC6q1xVFjVpmSmfZ7C7sALC3XKwJQZh1GOCvWBPrntX69O9GtNcHzuyhOfvH98n37g';
-const form = document.forms['submit-to-google-sheet'];
-const successMsg = document.getElementById("success-message");
-
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-      successMsg.style.display = "block";
-      form.reset();
-    })
-    .catch(error => console.error('Error!', error.message));
+    if (!carModel || !customerName || !phone || !diagnosticDate || !diagnosticTime || !service) {
+        event.preventDefault(); // Užkerta kelią formai būti išsiųstai
+        alert('Visi laukeliai turi būti užpildyti!');
+    } else {
+        alert('Jūsų rezervacija buvo sėkmingai pateikta!');
+    }
 });
